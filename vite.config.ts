b8d0f5c7641168290db58880639176c914f412aa
Akshar Patel <akshardpatel2004@gmail.com>
2025-03-31 23:45:39 -0400

    refactor: Update Vite configuration to specify output filename for the build

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,9 @@ export default defineConfig({
         noframes: true,
         "run-at": 'document-end'
       },
+      build: {
+        fileName: 'bilibili-subtitles.user.js',
+      }
     }),
     html(),
   ],
